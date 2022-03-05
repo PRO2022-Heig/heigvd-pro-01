0c245fdecad2ceb37a772617c7bb43a4cf083cf9
Simon Ackermann <simon.ackermann@heig-vd.ch>
2022-03-05 18:07:29 +0100

    [TASK] Move log and cache directories out of project root

--- app/backend/src/Kernel.php
+++ app/backend/src/Kernel.php
@@ -8,4 +8,14 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function getCacheDir(): string
+    {
+        return '/var/symfony/cache/';
+    }
+
+    public function getLogDir(): string
+    {
+        return '/var/symfony/log/';
+    }
 }
