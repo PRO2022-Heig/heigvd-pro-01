a66109dc1df382950f37c7f4688f587c314b6c3e
Simon Ackermann <simon.ackermann@heig-vd.ch>
2022-03-30 20:35:13 +0200

    Add test for Ingredient

--- app/backend/tests/Unit/Entity/IngredientTest.php
+++ app/backend/tests/Unit/Entity/IngredientTest.php
@@ -14,4 +14,18 @@ class IngredientTest extends KernelTestCase
         $ingredient = new Ingredient();
         $this->assertErrorCount(1, $ingredient->setName(""), "empty ingredient name");
     }
+
+    public function testGetId(): void
+    {
+        $ingredient = new Ingredient();
+        $this->assertEquals(0, $ingredient->getId(), "Id has problem");
+    }
+
+    public function testNameGetterAndSetter(): void
+    {
+        $testName = "test";
+        $ingredient = new Ingredient();
+        $ingredient->setName($testName);
+        $this->assertEquals($testName, $ingredient->getName(), "$testName does not match return");
+    }
 }
