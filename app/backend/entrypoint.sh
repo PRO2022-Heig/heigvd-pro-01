3b66186e9c6dc1f17686008b8231bae4724e36e6
David Gallay <davidtennis96@hotmail.com>
2022-05-28 18:15:46 +0200

    [FIX] Apparently, we need to have DATABASE_URL in the .env file

--- app/backend/entrypoint.sh
+++ app/backend/entrypoint.sh
@@ -5,7 +5,7 @@ test -f "/setup.sh" && /setup.sh
 echo "Waiting for database"
 /app/wait-for-it.sh -h database -p 3306 -t 90
 
-# echo "DATABASE_URL='$DATABASE_URL'" >> /app/.env
+echo "DATABASE_URL='$DATABASE_URL'" >> /app/.env
 
 echo "Seting up composer"
 cd /app && composer update && composer install && composer setup
