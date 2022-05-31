a9ebf609e49f002e49c66b0fc3debbbd25535fdb
Noah Boegli <39568628+noahboegli@users.noreply.github.com>
2022-06-01 00:04:13 +0200

    [BUGFIX] Added condition in index.php for production (change of working directory)

--- app/backend/public/index.php
+++ app/backend/public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__) . "/vendor/autoload_runtime.php";
 
+if ((getenv("APP_ENV") ?? "") === "prod") {
+    chdir("backend");
+}
+
 return function (array $context) {
     return new Kernel($context["APP_ENV"], (bool) $context["APP_DEBUG"]);
 };
